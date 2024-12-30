2bca414587da08790f1a1f016b4965d2551d2be3
lolomarka <lolomarka@gmail.com>
2024-12-30 06:36:42 +0500

    init

---
+++ config/projects/my_cpp_program.rb
@@ -0,0 +1,27 @@
+#
+# Copyright 2024 YOUR NAME
+#
+# All Rights Reserved.
+#
+
+name "my_cpp_program"
+maintainer "lolomarka <lolomarka@gmail.com>"
+homepage "https://github.com/lolomarka"
+
+# Defaults to C:/my_cpp_program on Windows
+# and /opt/my_cpp_program on all other platforms
+# install_dir "#{default_root}/#{name}"
+install_dir "/bin/#{name}"
+
+build_version Omnibus::BuildVersion.semver
+build_iteration 1
+
+# Creates required build directories
+dependency "preparation"
+dependency "gcc"
+
+# my_cpp_program dependencies/components
+# dependency "somedep"
+
+exclude "**/.git"
+exclude "**/bundler/git"
---
+++ config/software/my_cpp_program.rb
@@ -0,0 +1,8 @@
+name 'my_cpp_program'
+
+source path: "/home/lolomarka/Documents/Omnibus-my/my_cpp_program"
+default_version '1.0.0'
+
+build do
+    command "g++ main.cpp -o hello-world"
+end
