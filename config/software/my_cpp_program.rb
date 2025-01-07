4dd50ce3abc8f758b73073b6922feab82d76ffb8
lolomarka <lolomarka@gmail.com>
2025-01-07 17:19:15 +0500

    whitelist_file

--- config/software/my_cpp_program.rb
+++ config/software/my_cpp_program.rb
@@ -4,6 +4,8 @@ skip_transitive_dependency_licensing true
 source git: "https://github.com/lolomarka/my_cpp_program.git"
 # default_version '1.0.0'
 
+whitelist_file 'main.cpp'
+
 build do
     command "g++ main.cpp -o hello-world"
 end
