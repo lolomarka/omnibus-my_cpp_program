e34fc135b0d3debf09cd5200dac2c5c3547aacb7
lolomarka <lolomarka@gmail.com>
2025-01-07 16:35:53 +0500

    remove version

--- config/software/my_cpp_program.rb
+++ config/software/my_cpp_program.rb
@@ -1,7 +1,7 @@
 name 'my_cpp_program'
 
 source git: "https://github.com/lolomarka/my_cpp_program.git"
-default_version '1.0.0'
+# default_version '1.0.0'
 
 build do
     command "g++ main.cpp -o hello-world"
