16d94be245d909a4ee95ff12edd226300fe41615
lolomarka <lolomarka@gmail.com>
2025-01-08 11:25:31 +0500

    path in whitelist_file

--- config/software/my_cpp_program.rb
+++ config/software/my_cpp_program.rb
@@ -1,10 +1,10 @@
 name 'my_cpp_program'
-skip_transitive_dependency_licensing true
+skip_transitive_dependency_licensing true # отключает транзитивную проверку лицензий
+whitelist_file './main.cpp' 
 
 source git: "https://github.com/lolomarka/my_cpp_program.git"
 # default_version '1.0.0'
 
-whitelist_file 'main.cpp'
 
 build do
     command "g++ main.cpp -o hello-world"
