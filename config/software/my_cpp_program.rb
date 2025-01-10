99341f2d76925226f9ba09232327899b5ef234d3
lmrkv <34736486+lolomarka@users.noreply.github.com>
2025-01-10 09:23:05 +0500

    version

--- config/software/my_cpp_program.rb
+++ config/software/my_cpp_program.rb
@@ -2,9 +2,9 @@ name 'my_cpp_program'
 skip_transitive_dependency_licensing true # отключает транзитивную проверку лицензий
 whitelist_file './main.cpp' 
 
-default_version '0.0.0.1'
+default_version '0.0.0.2'
 
-version("0.0.0.1") { source sha1: "a4ca3836f68f59139ca40a70d734c273f81c0a6b" }
+version("0.0.0.2") { source sha1: "a4ca3836f68f59139ca40a70d734c273f81c0a6b" }
 
 source git: "https://github.com/lolomarka/my_cpp_program.git"
 
