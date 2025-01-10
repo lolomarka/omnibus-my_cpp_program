48aa515222948984331c29d7a59d2bd2c3e6d298
lmrkv <34736486+lolomarka@users.noreply.github.com>
2025-01-10 11:03:34 +0500

    adjust software definition

--- config/software/my_cpp_program.rb
+++ config/software/my_cpp_program.rb
@@ -9,6 +9,5 @@ version("0.0.0.2") { source sha1: "a4ca3836f68f59139ca40a70d734c273f81c0a6b" }
 source git: "https://github.com/lolomarka/my_cpp_program.git"
 
 build do
-    command "qmake"
     command "make"
 end
