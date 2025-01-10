72e2edf2eec05dbe509f3a7ec74c0e19076d05bf
lmrkv <34736486+lolomarka@users.noreply.github.com>
2025-01-10 08:58:03 +0500

    adjust software definition

--- config/software/my_cpp_program.rb
+++ config/software/my_cpp_program.rb
@@ -2,9 +2,11 @@ name 'my_cpp_program'
 skip_transitive_dependency_licensing true # отключает транзитивную проверку лицензий
 whitelist_file './main.cpp' 
 
-source git: "https://github.com/lolomarka/my_cpp_program.git"
 default_version '0.0.0.1'
 
+version("0.0.0.1") { source sha1: "a4ca3836f68f59139ca40a70d734c273f81c0a6b" }
+
+source git: "https://github.com/lolomarka/my_cpp_program.git"
 
 build do
     command "qmake"
