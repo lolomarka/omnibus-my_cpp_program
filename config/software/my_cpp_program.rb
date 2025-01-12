d7df5e9eff8de790d42eeac8789e968daa679baf
lolomarka <lolomarka@gmail.com>
2025-01-12 13:48:24 +0500

    new version

--- config/software/my_cpp_program.rb
+++ config/software/my_cpp_program.rb
@@ -2,13 +2,17 @@ name 'my_cpp_program'
 skip_transitive_dependency_licensing true # отключает транзитивную проверку лицензий
 whitelist_file './main.cpp' 
 
-default_version '0.0.0.2'
+default_version '0.0.0.3'
 
 version("0.0.0.2") { source sha1: "a4ca3836f68f59139ca40a70d734c273f81c0a6b" }
-
+version("0.0.0.3") { source sha1: "367c66711b453c2c86e794a8a889316afb75c584" }
 source git: "https://github.com/lolomarka/my_cpp_program.git"
 
 build do
-    command "cmake ./CMakeLists.txt"
-    command "make"
+    if version <= "0.0.0.2"
+        command "cmake ./CMakeLists.txt"
+        command "make -j #{workers}"
+    else
+        command "g++ ./main.cpp -o hello-world-app"
+    end
 end
